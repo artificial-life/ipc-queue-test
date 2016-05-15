69beda9f2c1ef6b96d2af5cd7f7018a259611859
artificial-life <proteus.protocol@gmail.com>
2016-05-15 17:41:32 +0600

    complete pub\sub and rpc example

--- pure/test2.js
+++ pure/test2.js
@@ -2,9 +2,13 @@
 let queue = require('global-queue');
 
 let Worker = require('./worker.js');
-let worker = new Worker('tcp://localhost:5671');
+let PubSub = require('./pubsub-adapter.js');
 
-queue.addAdapter('task', worker);
+let taskAdapter = new Worker('tcp://localhost:5671');
+let pubsubAdapter = new PubSub('tcp://127.0.0.1:5556', 'tcp://127.0.0.1:5555')
+
+queue.addAdapter('task', taskAdapter);
+queue.addAdapter('event', pubsubAdapter);
 
 queue.listenTask('other', (d) => {
 	return 'other'
@@ -17,3 +21,14 @@ queue.addTask('task1', {
 queue.addTask('other', {
 	p: 1
 }).then((d) => console.log(process.pid, d));
+
+queue.on('test-event', (d) => {
+	console.log(process.pid, 'rec event internal', d);
+});
+
+setTimeout(() => {
+	console.log(process.pid, 'emiting event');
+	queue.emit('test-event', {
+		test: 'data'
+	});
+}, 3000)
